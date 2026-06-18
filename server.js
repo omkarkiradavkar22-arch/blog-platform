ba4ccd3b9f8705a5c8c04d135a804eb349284411
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-06-19 02:11:30 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+const authRoutes = require('./routes/authRoutes');
+const blogRoutes = require('./routes/blogRoutes');
+const path = require('path');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+ app.get('/', (req, res) => {
+   res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use(session({
+    secret: 'secretKey',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// MongoDB Connection
+// mongoose.connect(process.env.MONGO_URI)
+// .then(() => console.log("MongoDB Connected"))
+// .catch(err => console.log(err));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log("MongoDB Error:", err);
+  });
+
+// Routes
+app.use('/auth', authRoutes);
+app.use('/blog', blogRoutes);
+
+const PORT=process.env.PORT||3000;
+app.listen(PORT, () => {
+    console.log("Server running on http://localhost:3000");
+});
