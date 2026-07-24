d9a638b2b2ac7469325358f28786867e49bc9cba
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-07-24 18:32:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,11 +23,6 @@ app.use(session({
     saveUninitialized: false
 }));
 
-// MongoDB Connection
-// mongoose.connect(process.env.MONGO_URI)
-// .then(() => console.log("MongoDB Connected"))
-// .catch(err => console.log(err));
-
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
